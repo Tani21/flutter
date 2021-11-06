d79e7704c6ee3a83a5fdbcd106376d173dc887df
snapsid <siddharthchopra555@gmail.com>
2021-11-06 15:35:11 +0530

    after signin bug fixed

--- google_fb/lib/home_page.dart
+++ google_fb/lib/home_page.dart
@@ -1,41 +1,30 @@
-import 'dart:html';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fb/Logged_In.dart';
 import 'package:google_fb/main.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          print("chal gaya");
 
-        if(snapshot.connectionState == ConnectionState.waiting)
-        {
-          return Center(child: CircularProgressIndicator());
-        }
-
-        else if(snapshot.hasData)
-        {
-          return HomePage();
-        }
-
-        else if(snapshot.hasError)
-        {
-          return Center(child: Text('Something went wrong'),);
-        }
-
-        
-
-        else 
-        {
-          return Text('dikkat h kuch to');
-        }
-      }
-    );
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            print("waiting");
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasData) {
+            return LoggedInWidget();
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Something went wrong'),
+            );
+          } else {
+            return Text('dikkat h kuch to');
+          }
+        });
   }
 }
